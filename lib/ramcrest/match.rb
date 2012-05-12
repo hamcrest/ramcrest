5947cbe12a53f935562f2cccbdfd44c7e0c0fddd
Andrew Parker <aparker42@gmail.com>
2012-05-12 13:56:37 -0700

    Created Match class
    This class will hold the result of a match. It will include the
    description as well as the true or false value of the match.

---
+++ lib/ramcrest/match.rb
@@ -0,0 +1,24 @@
+module Ramcrest
+  class Match
+    attr_reader :description
+
+    def self.success
+      Match.new(true, nil)
+    end
+
+    def self.mismatched(description) 
+      Match.new(false, description)
+    end
+
+    def matched?
+      @matched
+    end
+
+    private
+    
+    def initialize(matched, description)
+      @matched = matched
+      @description = description
+    end
+  end
+end
---
+++ test/match_test.rb
@@ -0,0 +1,20 @@
+require 'minitest/autorun'
+require 'ramcrest/match'
+
+describe Ramcrest::Match do
+  it "a successful match is equivalent to true" do
+    Ramcrest::Match.success.matched?.must_equal true
+  end
+
+  it "an unsuccessful match is equivalent to false" do
+    Ramcrest::Match.mismatched("description").matched?.must_equal false
+  end
+
+  it "an unsuccessful match has a description" do
+    Ramcrest::Match.mismatched("description").description.must_equal "description"
+  end
+
+  it "a successful match has no description" do
+    Ramcrest::Match.success.description.must_be_nil
+  end
+end
