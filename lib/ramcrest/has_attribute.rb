4f9463a5ce750d83df495670d52d29a0a77d9ca2
Andrew Parker <aparker42@gmail.com>
2012-05-12 14:48:02 -0700

    Allow has_attribute to match value
    The has_attribute matcher can be composed with other matchers to match
    the value of the attribute.

--- lib/ramcrest/has_attribute.rb
+++ lib/ramcrest/has_attribute.rb
@@ -1,26 +1,37 @@
 require 'ramcrest/match'
+require 'ramcrest/anything'
 
 module Ramcrest
   module HasAttribute
-    def has_attribute(attribute_name)
-      HasAttribute.new(attribute_name)
+    def has_attribute(attribute_name, value_matcher = anything())
+      HasAttribute.new(attribute_name, value_matcher)
     end
 
+    private
+
+    include Ramcrest::Anything
+
     class HasAttribute
-      def initialize(attribute_name)
+      def initialize(attribute_name, value_matcher)
         @attribute_name = attribute_name
+        @value_matcher = value_matcher
       end
 
       def matches?(actual)
         if actual.respond_to?(@attribute_name)
-          return Ramcrest::Match.success
+          match = @value_matcher.matches?(actual.send(@attribute_name))
+          if match.matched?
+            return Ramcrest::Match.success
+          else
+            return Ramcrest::Match.mismatch("object <#{actual}> attribute '#{@attribute_name}' #{match.description}")
+          end
         else 
           return Ramcrest::Match.mismatch("object <#{actual}> was missing attribute '#{@attribute_name}'")
         end
       end
 
       def description
-        "an object with an attribute named '#{@attribute_name}'"
+        "an object with an attribute named '#{@attribute_name}' and value #{@value_matcher.description}"
       end
     end
   end
--- test/has_attribute_test.rb
+++ test/has_attribute_test.rb
@@ -1,36 +1,77 @@
 require 'minitest/autorun'
 require 'ramcrest/has_attribute'
+require 'ramcrest/is'
 
 describe Ramcrest::HasAttribute do
   include Ramcrest::HasAttribute
+  include Ramcrest::Is
 
   describe "when not matching the value" do
     it "matches an object with the attribute" do
       obj = an_object_with_attribute(:foo)
 
-      has_attribute(:foo).matches?(obj).matched?.must_equal true
+      has_attribute(:foo).matches?(obj).
+        matched?.must_equal true
     end
 
     it "does not match an object that does not have the attribute" do
       obj = an_object_with_attribute(:different)
 
-      has_attribute(:foo).matches?(obj).matched?.must_equal false
+      has_attribute(:foo).matches?(obj).
+        matched?.must_equal false
     end
 
     it "describes why it does not match an object that is missing the attribute" do
       obj = an_object_with_attribute(:different)
 
-      has_attribute(:foo).matches?(obj).description.must_equal "object <#{obj}> was missing attribute 'foo'"
+      has_attribute(:foo).matches?(obj).
+        description.must_equal "object <#{obj}> was missing attribute 'foo'"
     end
 
     it "describes itself" do
-      has_attribute(:foo).description.must_equal "an object with an attribute named 'foo'"
+      has_attribute(:foo).
+        description.must_equal "an object with an attribute named 'foo' and value anything"
     end
   end
 
-  def an_object_with_attribute(name)
+  describe "when matching the value" do
+    it "matches an object with the attribute and the value" do
+      obj = an_object_with_attribute(:foo, 1)
+
+      has_attribute(:foo, is(1)).matches?(obj).
+        matched?.must_equal true
+    end
+
+    it "does not match an object that does not have the attribute" do
+      obj = an_object_with_attribute(:different)
+
+      has_attribute(:foo, is(1)).matches?(obj).
+        matched?.must_equal false
+    end
+
+    it "does not match an object that has a different value" do
+      obj = an_object_with_attribute(:foo, 2)
+
+      has_attribute(:foo, is(1)).matches?(obj).
+        matched?.must_equal false
+    end
+
+    it "describes why it does not match an object that is missing the attribute" do
+      obj = an_object_with_attribute(:different)
+
+      has_attribute(:foo, is(1)).matches?(obj).
+        description.must_equal "object <#{obj}> was missing attribute 'foo'"
+    end
+
+    it "describes itself" do
+      has_attribute(:foo, is(1)).
+        description.must_equal "an object with an attribute named 'foo' and value is <1>"
+    end
+  end
+
+  def an_object_with_attribute(name, value = nil)
     obj_with_attr = Object.new
-    obj_with_attr.define_singleton_method(name) {}
+    obj_with_attr.define_singleton_method(name) { value }
     obj_with_attr
   end
 end
