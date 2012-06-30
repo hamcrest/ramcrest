f4cf68725b2da943dcfe88b52ac88a35ce7823b6
Andrew Parker <aparker42@gmail.com>
2012-06-30 10:25:46 -0700

    Refactored is() tests to use the matcher matcher

--- test/is_test.rb
+++ test/is_test.rb
@@ -2,21 +2,18 @@ require 'minitest/autorun'
 require 'ramcrest'
 
 describe Ramcrest::Is do
+  include Ramcrest::MatcherMatcher
   include Ramcrest::Is
 
   it "matches objects that are the same" do
-    is(1).matches?(1).matched?.must_equal true
+    assert_that is(1), a_matcher_that_matches(1)
   end
 
   it "does not match objects that are different" do
-    is(1).matches?(2).matched?.must_equal false
-  end
-
-  it "describes the object that was seen when it mismatches" do
-    is(1).matches?(2).description.must_equal "was <2>" 
+    assert_that is(1), a_matcher_that_mismatches(2, "was <2>")
   end
 
   it "describes itself" do
-    is(1).description.must_equal "is <1>" 
+    assert_that is(1), a_matcher_described_as("is <1>")
   end
 end
